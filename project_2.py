0f9437d1ac7fc2eca7f3bd659835002dfd5a0fcc
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2024-11-21 21:53:59 -0400

    completed

--- project_2.py
+++ project_2.py
@@ -1,6 +1,7 @@
 import sys
 import csv
 import math
+import array
 
 class BloomFilter:
 
@@ -9,7 +10,51 @@ class BloomFilter:
     self.bt_size, self.num_hashes = self.calculateBloomParameters(element_count, 0.0000001)
     
     # Allocate the necessary bits
-    self.cache_filter = [0] * self.bt_size
+    self.bit_array = self.makeBitArray(self.bt_size, fill=0)
+
+  def makeBitArray(self, bitSize, fill = 0):
+    intSize = bitSize >> 5      # number of 32 bit integers
+    if (bitSize & 31):          # if bitSize != (32 * n) add
+        intSize += 1            #    a record for stragglers
+    if fill == 1:    
+        fill = 4294967295       # all bits set == 2^32
+    else:    
+        fill = 0                # all bits cleared
+
+    bitArray = array.array('I') # 'I' = unsigned 32-bit integer
+    bitArray.extend((fill,) * intSize)
+    return(bitArray)
+
+  # testBit() returns a nonzero result, 2**offset, if the bit at 'bit_num' is set to 1.
+  def testBit(self, bit_num):
+    record = bit_num >> 5
+    offset = bit_num & 31
+    mask = 1 << offset
+    return(self.bit_array[record] & mask)
+
+  # setBit() returns an integer with the bit at 'bit_num' set to 1.
+  def setBit(self, bit_num):
+    record = bit_num >> 5
+    offset = bit_num & 31
+    mask = 1 << offset
+    self.bit_array[record] |= mask
+    return(self.bit_array[record])
+
+  # clearBit() returns an integer with the bit at 'bit_num' cleared.
+  def clearBit(self, bit_num):
+    record = bit_num >> 5
+    offset = bit_num & 31
+    mask = ~(1 << offset)
+    self.bit_array[record] &= mask
+    return(self.bit_array[record])
+
+  # toggleBit() returns an integer with the bit at 'bit_num' inverted, 0 -> 1 and 1 -> 0.
+  def toggleBit(self, bit_num):
+    record = bit_num >> 5
+    offset = bit_num & 31
+    mask = 1 << offset
+    self.bit_array[record] ^= mask
+    return(self.bit_array[record])
 
   # Parameters:
   # n == count of elements from hash set
@@ -32,46 +77,17 @@ class BloomFilter:
     hashes = [self.hash(i * i, value) % self.bt_size for i in range(0, self.num_hashes)]
     
     for index in hashes:
-      self.cache_filter[index] = 1 # set as active
+      self.setBit(index)
 
   # Check if the value is probably in filter
   def check(self, value):
     hashes = [self.hash(i * i, value) % self.bt_size for i in range(0, self.num_hashes)]
 
     for index in hashes:
-      if self.cache_filter[index] == 0:
+      if self.testBit(index) == 0:
         return False # Is definitely NOT in database
     
     return True # Probably in database
-  
-class HashTableSet:
-
-  # Create the Set with N ammount of elements
-  # Start with an empty set of already allocated spaces
-  def __init__(self, element_count):
-    # Create a filter
-    self.filter = BloomFilter(element_count)
-    # Set hashtable-set
-    self.data = [None] * element_count
-
-  # Adds a value to the set
-  def add(self, value):
-    # Obtain the index of the element in hash table
-    index = self.filter.hash(1, value) % len(self.data)
-
-    # If element exists, dont add it
-    if self.filter.check(value):
-      return 
-
-    # If element doesnt exist, insert it
-    self.data[index] = value
-    self.filter.cache(value)
-
-  # Returns the corresponding value from table
-  def tableCheck(self, value):
-    if not self.filter.check(value):
-      return False # Not in hash table
-    return True
   
 def readCSV(file_path):
   with open(file_path, 'r') as file:
@@ -81,15 +97,15 @@ def main(input_1, input_2):
   database = readCSV(input_1)
   database_request = readCSV(input_2)
 
-  # Create a new database with a preset size
-  database_set = HashTableSet(len(database))
+  # Create the filter for testing
+  filter = BloomFilter(len(database))
 
   # Add data to database set
   for element in database:
-    database_set.add(element)
+    filter.cache(element)
 
   for element in database_request:
-    if database_set.tableCheck(element):
+    if filter.check(element):
       print(f'{element},Probably in the DB')
     else:
       print(f'{element},Not in the DB')
